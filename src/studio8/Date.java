ee6ec909451f4e8ddf7b65e4e078233b29e80880
josephguo <josephguo@localhost>
2023-04-06 15:46:16 -0500

    Studio 8_JosephAnna

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,10 +1,44 @@
 package studio8;
 
-public class Date {
+import java.util.Objects;
 
+public class Date {
+	private int month;
+	private int day;
+	private int year;
+	
+	public Date (int month, int day, int year) {
+		this.month=month;
+		this.day=day;
+		this.year=year;
+	}
+
+	public String toString() {
+		
+		return month+ "/" + day + "/" + year;
+	}
 
     public static void main(String[] args) {
+    	Date today= new Date (4, 6, 2023);
+    	System.out.println(today);
 
     }
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(day, month, year);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Date other = (Date) obj;
+		return day == other.day && month == other.month && year == other.year;
+	}
 
 }
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -1,11 +1,55 @@
 package studio8;
 
+import java.util.Objects;
 
 public class Time {
+private int hour;
+private int minute;
+private boolean isTwelveHourFormat;
 
+public Time (int hour, int minute, boolean isTwelveHourFormat) {
+	this.hour=hour;
+	this.minute=minute;
+	this.isTwelveHourFormat=isTwelveHourFormat;
+}
+	
+public String toString() {
+	if (isTwelveHourFormat) {
+		return this.hour % 12 + ":" +this.minute;
+	}
+	else {
+		return this.hour+ ":" + this.minute;
+	}
+}
+	
+	
 	public static void main(String[] args) {
- 
+		Time twelve = new Time(15, 20, true);
+		Time twentyFour = new Time(15, 20, false);
     	
+		System.out.print(twelve+" ");
+		System.out.println(twelve.equals(twelve));
+		System.out.print(twentyFour+" ");
+		System.out.println(twentyFour.equals(twelve));
     }
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(hour, isTwelveHourFormat, minute);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Time other = (Time) obj;
+		return hour == other.hour && isTwelveHourFormat == other.isTwelveHourFormat && minute == other.minute;
+	}
+	
+
 
 }
